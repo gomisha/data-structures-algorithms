356389bb2f13dbd7e71b6087bd22b244567bcae9
Misha <15269764+gomisha@users.noreply.github.com>
2025-07-07 06:38:36 -0400

    var name changes

--- binary-search/go/binary_search.go
+++ binary-search/go/binary_search.go
@@ -4,29 +4,31 @@ import "fmt"
 
 func main() {
 	// write binary search algorithm
-	target := 3
+	target := 5
 	result, counter := binarySearch([]int{1, 2, 3, 4, 5}, target)
 
 	fmt.Println("target: ", target, " counter: ", counter, " result: ", result)
 }
 
 func binarySearch(array []int, target int) (int, int) {
-	left := 0
-	right := len(array) - 1
+	low := 0
+	high := len(array) - 1
 	counter := 1
-	for left <= right {
-		mid := left + (right-left)/2
-		if array[mid] == target {
+	for low <= high {
+		//mid := low + (high-low)/2
+		mid := (low + high) / 2
+		guess := array[mid]
+		if guess == target {
 			return mid, counter
 		}
-		if array[mid] < target {
-			left = mid + 1
+		if guess < target {
+			low = mid + 1
 			counter++
-			continue
+			//continue
 		}
-		if array[mid] > target {
+		if guess > target {
 			counter++
-			right = mid - 1
+			high = mid - 1
 		}
 	}
 	return -1, counter
