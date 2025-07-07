5e4c0ae764dfb4086d365c244d2d231633f4276d
Misha <15269764+gomisha@users.noreply.github.com>
2025-07-07 07:00:55 -0400

    Refactor binary search and update test case
    Updated the binary search implementation to increment the counter only once per iteration and simplified the conditional logic. Changed the test case in main to search for 20 in a larger array.

--- binary-search/go/binary_search.go
+++ binary-search/go/binary_search.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// write binary search algorithm
-	target := 5
-	result, counter := binarySearch([]int{1, 2, 3, 4, 5}, target)
+	target := 20
+	result, counter := binarySearch([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, target)
 
 	fmt.Println("target: ", target, " counter: ", counter, " result: ", result)
 }
@@ -13,22 +12,17 @@ func main() {
 func binarySearch(array []int, target int) (int, int) {
 	low := 0
 	high := len(array) - 1
-	counter := 1
+	counter := 0
 	for low <= high {
-		//mid := low + (high-low)/2
+		counter++
 		mid := (low + high) / 2
 		guess := array[mid]
 		if guess == target {
 			return mid, counter
-		}
-		if guess < target {
-			low = mid + 1
-			counter++
-			//continue
-		}
-		if guess > target {
-			counter++
+		} else if guess > target {
 			high = mid - 1
+		} else if guess < target {
+			low = mid + 1
 		}
 	}
 	return -1, counter
